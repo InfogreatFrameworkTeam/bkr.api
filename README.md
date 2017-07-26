a205fc84168911e25c74818b8e02dfdf7a539803
chengd <chengd@chengd.infogreat.cn>
2017-07-26 10:24:07 +0800

    result的package不太合适 #2

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 2.bkr.base 执行 maven.install
 3.bkr.core 执行 maven.install
 4.确认bkr。api的src\main\webapp\WEB-INF\lib目录下 bkr-base.jar， bkr-core.jar是否生成
-5.D:\brk_sapce\bkr.api\src\main\java\bkr\BkrApplication.java run as Java Application
+5.bkr.api  执行 maven.update
+6.D:\brk_sapce\bkr.api\src\main\java\bkr\BkrApplication.java run as Java Application
--- src/main/java/bkr/api/controller/session/LoginController.java
+++ src/main/java/bkr/api/controller/session/LoginController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import bkr.base.result.JsonResult;
-import bkr.base.result.ResultCode;
+import bkr.base.api.result.JsonResult;
+import bkr.base.api.result.ResultCode;
 import bkr.base.util.string.StringUtil;
 import bkr.core.data.dto.UserDto;
 import bkr.core.service.session.LoginService;
